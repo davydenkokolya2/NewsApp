fab50083a0f78cb07851a38d80a08e237713d18a
nikolajdavydenko <davydenko.kolya@bk.ru>
2023-10-03 18:53:33 +0300

    project init

---
+++ app/src/main/java/com/example/newsapp/ui/news_details/NewsDetailsFragment.kt
@@ -0,0 +1,32 @@
+package com.example.newsapp.ui.news_details
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsapp.R
+
+class NewsDetailsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = NewsDetailsFragment()
+    }
+
+    private lateinit var viewModel: NewsDetailsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_news_details, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(NewsDetailsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/newsapp/ui/news_details/NewsDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.newsapp.ui.news_details
+
+import androidx.lifecycle.ViewModel
+
+class NewsDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
